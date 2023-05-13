79724f503c7969ae0e5edaea353615d705cd53f8
Diego Teixeira <diegosmteixeira@gmail.com>
2023-05-13 15:50:31 -0300

    Add exceptions filter and dependency injection

---
+++ Backend/src/Layers/RecipeBook.API/Filters/ExceptionsFilter.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using RecipeBook.Communication.Response;
+using RecipeBook.Exception;
+using RecipeBook.Exception.ExceptionsBase;
+using System.Net;
+
+namespace RecipeBook.API.Filters;
+
+public class ExceptionsFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is RecipeBookException)
+        {
+            PersonalizeException(context);
+        }
+        else
+        {
+            ThrowUnknowException(context);
+        }
+    }
+
+    private void PersonalizeException(ExceptionContext context)
+    {
+        if (context.Exception is ValidatorErrorsException)
+        {
+            ExceptionHandler(context);
+        }
+    }
+    private void ExceptionHandler(ExceptionContext context)
+    {
+        var validatorErrors = context.Exception as ValidatorErrorsException;
+
+        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+        context.Result = new ObjectResult(new ErrorResponseJson(validatorErrors.ErrorMessages));
+    }
+
+    private void ThrowUnknowException(ExceptionContext context)
+    {
+        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        context.Result = new ObjectResult(new ErrorResponseJson(ResourceErrorMessages.UNKNOWN_ERROR));
+    }
+
+
+}
--- Backend/src/Layers/RecipeBook.API/Program.cs
+++ Backend/src/Layers/RecipeBook.API/Program.cs
@@ -1,3 +1,6 @@
+using RecipeBook.API.Filters;
+using RecipeBook.Application.Services;
+using RecipeBook.Application.Services.AutoMapper;
 using RecipeBook.Domain.Extension;
 using RecipeBook.Infrastructure;
 using RecipeBook.Infrastructure.Migrations;
@@ -12,6 +15,14 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddRepository(builder.Configuration);
+builder.Services.AddApplication(builder.Configuration);
+
+builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionsFilter)));
+
+builder.Services.AddScoped(provider => new AutoMapper.MapperConfiguration(config =>
+{
+    config.AddProfile(new AutoMapperConfiguration());
+}).CreateMapper());
 
 var app = builder.Build();
 
