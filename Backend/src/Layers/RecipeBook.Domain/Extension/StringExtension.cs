b8ef15026e0126c991c78e9e88c75abc6132042f
Diego Teixeira <diegosmteixeira@gmail.com>
2023-05-30 14:43:56 -0300

    fix: add custom comparer method and accent remover to title and ingredients filters (recipe validator and dashboard usecase)

---
+++ Backend/src/Layers/RecipeBook.Domain/Extension/StringExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace RecipeBook.Domain.Extension;
+public static class StringExtension
+{
+    public static bool CustomComparer(this string source, string search)
+    {
+        var index = CultureInfo.CurrentCulture.CompareInfo
+            .IndexOf(source, search, CompareOptions.IgnoreCase | CompareOptions.IgnoreNonSpace);
+
+        return index >= 0;
+    }
+
+    public static string AccentRemover(this string source)
+    {
+        return new string(source.Normalize(NormalizationForm.FormD)
+            .Where(ch => char.GetUnicodeCategory(ch) != UnicodeCategory.NonSpacingMark)
+            .ToArray());
+    }
+}
